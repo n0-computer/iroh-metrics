79916f74e6dc42e19a41318b1ea148f63fad3490
dignifiedquire <dignifiedquire@users.noreply.github.com>
2024-12-13 19:08:22 +0000

    deploy: 1064985540ecbd0c3cecd58986a52fb4d70f7d61

---
+++ pr/3/docs/trait.impl/core/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_metrics":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"iroh_metrics/enum.Error.html\" title=\"enum iroh_metrics::Error\">Error</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ pr/3/docs/trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_metrics":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"iroh_metrics/enum.Error.html\" title=\"enum iroh_metrics::Error\">Error</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
