fd85fad9ee96545ba324664950e2ef2046799483
Paul Olteanu <p.a.olteanu@gmail.com>
2025-01-08 22:41:56 -0500

    Add support for Gauge metrics

--- src/lib.rs
+++ src/lib.rs
@@ -34,7 +34,7 @@ macro_rules! inc {
     };
 }
 
-/// Increment the given counter `n`.
+/// Increment the given counter by `n`.
 #[macro_export]
 macro_rules! inc_by {
     ($m:ty, $f:ident, $n:expr) => {
@@ -45,6 +45,46 @@ macro_rules! inc_by {
 /// Set the given counter to `n`.
 #[macro_export]
 macro_rules! set {
+    ($m:ty, $f:ident, $n:expr) => {
+        <$m as $crate::core::Metric>::with_metric(|m| m.$f.set($n));
+    };
+}
+
+/// Increment the given gauge by 1.
+#[macro_export]
+macro_rules! inc_gauge {
+    ($m:ty, $f:ident) => {
+        <$m as $crate::core::Metric>::with_metric(|m| m.$f.inc());
+    };
+}
+
+/// Increment the given gauge by `n`.
+#[macro_export]
+macro_rules! inc_gauge_by {
+    ($m:ty, $f:ident, $n:expr) => {
+        <$m as $crate::core::Metric>::with_metric(|m| m.$f.inc_by($n));
+    };
+}
+
+/// Decrement the given gauge by 1.
+#[macro_export]
+macro_rules! dec_gauge {
+    ($m:ty, $f:ident) => {
+        <$m as $crate::core::Metric>::with_metric(|m| m.$f.dec());
+    };
+}
+
+/// Decrement the given gauge `n`.
+#[macro_export]
+macro_rules! dec_gauge_by {
+    ($m:ty, $f:ident, $n:expr) => {
+        <$m as $crate::core::Metric>::with_metric(|m| m.$f.dec_by($n));
+    };
+}
+
+/// Set the given gauge to `n`.
+#[macro_export]
+macro_rules! set_gauge {
     ($m:ty, $f:ident, $n:expr) => {
         <$m as $crate::core::Metric>::with_metric(|m| m.$f.set($n));
     };
