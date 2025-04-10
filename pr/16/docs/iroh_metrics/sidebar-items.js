68d93549fc3504e4c9b813e40ca6425eaf014e4c
Frando <Frando@users.noreply.github.com>
2025-04-10 10:19:23 +0000

    deploy: 7cb605dcef9bfd6bc7a6432dabdf8df2533640e4

---
+++ pr/16/docs/iroh_metrics/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"derive":["MetricsGroup"],"enum":["Error","MetricType","MetricValue"],"fn":["parse_prometheus_metrics"],"macro":["dec","dec_by","inc","inc_by","set"],"mod":["service","static_core"],"struct":["Counter","Gauge","MetricDescription","MetricItem","Registry","ValuesIter"],"trait":["MetricsGroup","MetricsGroupExt","MetricsGroupSet"]};
---
+++ pr/16/docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_metrics",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"iroh_metrics/struct.ValuesIter.html\" title=\"struct iroh_metrics::ValuesIter\">ValuesIter</a>&lt;'_&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[339]}
