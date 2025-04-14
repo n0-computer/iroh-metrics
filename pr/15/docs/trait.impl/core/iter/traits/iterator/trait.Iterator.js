7c430dfabe7f3d22033d02ae07a3487a6d883e88
Frando <Frando@users.noreply.github.com>
2025-04-14 10:08:11 +0000

    deploy: 007fcffbd3b62f89d6e4e59e6eaffe74b0eb2df9

--- pr/15/docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++ pr/15/docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh_metrics",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"iroh_metrics/struct.ValuesIter.html\" title=\"struct iroh_metrics::ValuesIter\">ValuesIter</a>&lt;'_&gt;"]]]]);
+    var implementors = Object.fromEntries([["iroh_metrics",[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"iroh_metrics/iterable/struct.FieldIter.html\" title=\"struct iroh_metrics::iterable::FieldIter\">FieldIter</a>&lt;'a&gt;"],["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"iroh_metrics/struct.MetricsIter.html\" title=\"struct iroh_metrics::MetricsIter\">MetricsIter</a>&lt;'a&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[339]}
+//{"start":57,"fragment_lengths":[699]}
