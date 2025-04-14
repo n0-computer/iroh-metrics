57fe1bd30fbcfdec27451ba2ecff68d02d0f81b4
Frando <Frando@users.noreply.github.com>
2025-04-14 10:36:56 +0000

    deploy: 3273e34dfa0f65f00cd98a1bcd14e2f5b88d559e

---
+++ pr/20/docs/src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["iroh_metrics",["",[],["base.rs","iterable.rs","lib.rs","metrics.rs","service.rs","static_core.rs"]]],["iroh_metrics_derive",["",[],["lib.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[102,43]}
---
+++ pr/20/docs/trait.impl/core/ops/deref/trait.Deref.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_metrics",[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"iroh_metrics/struct.MetricItem.html\" title=\"struct iroh_metrics::MetricItem\">MetricItem</a>&lt;'a&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[317]}
