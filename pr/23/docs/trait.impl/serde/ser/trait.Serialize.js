a3232839ae8fe3724d8813791ebf0773ea0580c6
Frando <Frando@users.noreply.github.com>
2025-04-28 08:50:39 +0000

    deploy: 908b1bc5c340765ea0e52be8dab796c0161781cf

---
+++ pr/23/docs/trait.impl/serde/ser/trait.Serialize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_metrics",[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.216/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"iroh_metrics/struct.Counter.html\" title=\"struct iroh_metrics::Counter\">Counter</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.216/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"iroh_metrics/struct.Gauge.html\" title=\"struct iroh_metrics::Gauge\">Gauge</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[546]}
