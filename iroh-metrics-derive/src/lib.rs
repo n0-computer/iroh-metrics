98c5e2e1e181201f2d25bf9f42b596126ab46c25
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2025-12-18 15:39:05 +0100

    smooth out usage of labels

--- iroh-metrics-derive/src/lib.rs
+++ iroh-metrics-derive/src/lib.rs
@@ -2,8 +2,8 @@ use heck::ToSnakeCase;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{
-    Attribute, Data, DeriveInput, Error, Expr, ExprLit, Fields, Ident, Lit, LitStr,
-    meta::ParseNestedMeta, parse_macro_input, spanned::Spanned,
+    Attribute, Data, DeriveInput, Error, Expr, ExprLit, Fields, GenericArgument, Ident, Lit,
+    LitStr, PathArguments, Type, meta::ParseNestedMeta, parse_macro_input, spanned::Spanned,
 };
 
 #[proc_macro_derive(MetricsGroup, attributes(metrics, default))]
@@ -41,10 +41,16 @@ pub fn derive_encode_label_set(input: TokenStream) -> TokenStream {
 fn expand_iterable(input: &DeriveInput) -> Result<proc_macro2::TokenStream, Error> {
     let (name, fields) = parse_named_struct(input)?;
 
-    let count = fields.len();
+    // Separate regular metrics and Family fields
+    let (metric_fields, family_fields): (Vec<_>, Vec<_>) =
+        fields.iter().partition(|f| !is_family_type(&f.ty));
 
-    let mut match_arms = quote! {};
-    for (i, field) in fields.iter().enumerate() {
+    let metric_count = metric_fields.len();
+    let family_count = family_fields.len();
+
+    // Generate match arms for regular metrics
+    let mut metric_match_arms = quote! {};
+    for (i, field) in metric_fields.iter().enumerate() {
         let ident = field.ident.as_ref().unwrap();
         let ident_str = ident.to_string();
         let attr = parse_metrics_attr(&field.attrs)?;
@@ -52,25 +58,79 @@ fn expand_iterable(input: &DeriveInput) -> Result<proc_macro2::TokenStream, Erro
             .help
             .or_else(|| parse_doc_first_line(&field.attrs))
             .unwrap_or_else(|| ident_str.clone());
-        match_arms.extend(quote! {
+        metric_match_arms.extend(quote! {
             #i => Some(::iroh_metrics::MetricItem::new(#ident_str, #help, &self.#ident as &dyn ::iroh_metrics::Metric)),
         });
     }
 
-    Ok(quote! {
-        impl ::iroh_metrics::iterable::Iterable for #name {
-            fn field_count(&self) -> usize {
-                #count
+    // Generate match arms for Family fields
+    let mut family_match_arms = quote! {};
+    for (i, field) in family_fields.iter().enumerate() {
+        let ident = field.ident.as_ref().unwrap();
+        let ident_str = ident.to_string();
+        let attr = parse_metrics_attr(&field.attrs)?;
+        let help = attr
+            .help
+            .or_else(|| parse_doc_first_line(&field.attrs))
+            .unwrap_or_else(|| ident_str.clone());
+        family_match_arms.extend(quote! {
+            #i => Some(::iroh_metrics::FamilyItem::new(#ident_str, #help, &self.#ident as &dyn ::iroh_metrics::FamilyEncoder)),
+        });
+    }
+
+    let family_impl = if family_count > 0 {
+        quote! {
+            fn family_count(&self) -> usize {
+                #family_count
             }
 
-            fn field_ref(&self, n: usize) -> Option<::iroh_metrics::MetricItem<'_>> {
+            fn family_ref(&self, n: usize) -> Option<::iroh_metrics::FamilyItem<'_>> {
                 match n {
-                    #match_arms
+                    #family_match_arms
                     _ => None,
                 }
             }
         }
+    } else {
+        quote! {}
+    };
+
+    Ok(quote! {
+        impl ::iroh_metrics::iterable::Iterable for #name {
+            fn field_count(&self) -> usize {
+                #metric_count
+            }
+
+            fn field_ref(&self, n: usize) -> Option<::iroh_metrics::MetricItem<'_>> {
+                match n {
+                    #metric_match_arms
+                    _ => None,
+                }
+            }
+
+            #family_impl
+        }
     })
+}
+
+/// Checks if a type is `Family<_, _>`.
+fn is_family_type(ty: &Type) -> bool {
+    if let Type::Path(type_path) = ty {
+        if let Some(segment) = type_path.path.segments.last() {
+            if segment.ident == "Family" {
+                if let PathArguments::AngleBracketed(args) = &segment.arguments {
+                    // Family should have 2 generic arguments
+                    return args
+                        .args
+                        .iter()
+                        .filter(|arg| matches!(arg, GenericArgument::Type(_)))
+                        .count()
+                        == 2;
+                }
+            }
+        }
+    }
+    false
 }
 
 fn expand_metrics(input: &DeriveInput) -> Result<proc_macro2::TokenStream, Error> {
