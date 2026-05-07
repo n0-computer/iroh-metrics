9a6bc81cc6bdb48e5ee7f1aeb1bdaf4297d0517d
Arqu <Arqu@users.noreply.github.com>
2026-05-07 10:13:18 +0000

    deploy: d5edd8b63baf2558aa794154e0581c272b7efc2b

---
+++ pr/112/docs/trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_metrics",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"iroh_metrics/enum.Error.html\" title=\"enum iroh_metrics::Error\">Error</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[269]}
